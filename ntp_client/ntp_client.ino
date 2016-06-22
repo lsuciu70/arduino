63960162fb3b02f9806903eabd1ad9b27296425b
Liviu Suciu <liviu.suciu@nokia.com>
2016-06-22 16:05:27 +0300

    ntp_client

--- ntp_client/ntp_client.ino
+++ ntp_client/ntp_client.ino
@@ -1,18 +1,26 @@
 #include <time.h>
-#include <WiFiUdp.h>
-#include <ESP8266WiFi.h>
-#include <Time.h>
-#include <Timezone.h>
+#include <WiFiUdp.h>      // http://playground.arduino.cc/Code/NTPclient
+#include <ESP8266WiFi.h>  // https://github.com/esp8266/Arduino
+#include <Time.h>         // https://github.com/PaulStoffregen/Time
+#include <Timezone.h>     // https://github.com/JChristensen/Timezone
 
 // Eastern European Time (Timisoara)
-TimeChangeRule EEST = {"EEST", Last, Sun, Mar, 3, 120}; // Eastern European Summer Time
-TimeChangeRule EET  = {"EET",  Last, Sun, Oct, 4,  60}; // Eastern European Standard Time
-Timezone EE(EEST, EET);
+const TimeChangeRule EEST = {"EEST", Last, Sun, Mar, 3, 120}; // Eastern European Summer Time
+const TimeChangeRule EET  = {"EET",  Last, Sun, Oct, 4,  60}; // Eastern European Standard Time
+Timezone EasternEuropeanTime(EEST, EET);
 
 TimeChangeRule *tcr;
 
-const char* ssid     = "lsu-phone";
-const char* password = "r4cD7TPG";
+const char* SSID_t   = "lsu-phone";
+const char* PASSWD_t = "r4cD7TPG";
+
+// Constants waiting for NTP server response; check every POLL_INTERVAL (ms) up to POLL_TIMES times
+const int POLL_INTERVAL = 150; // poll every this many ms
+const byte POLL_TIMES = 15;  // poll up to this many times
+// NTP packet length
+const int PKT_LEN = 48;
+// Useless bytes to be discarded; set useless to 32 for speed; set to 40 for accuracy.
+const byte USELES_BYTES = 40;
 
 time_t getTime();
 void timeDisplay();
@@ -22,44 +30,22 @@ void setup()
   // put your setup code here, to run once:
   Serial.begin(115200);
   delay(100);
-  if(timeStatus() == timeNotSet)
-    Serial.println("Time not synched");
-  setSyncProvider(getTime);// Set the external time provider
-  setSyncInterval(10); 
-  //
-  //  // We start by connecting to a WiFi network
-  //
-  //  Serial.println();
-  //  Serial.println();
-  //  Serial.print("Connecting to ");
-  //  Serial.println(ssid);
-  //
-  //  WiFi.begin(ssid, password);
-  //
-  //  while (WiFi.status() != WL_CONNECTED) {
-  //    delay(500);
-  //    Serial.print(".");
-  //  }
-  //
-  //  Serial.println("");
-  //  Serial.println("WiFi connected");
+  // Set the external time provider
+  setSyncProvider(getTime);
+  // Set synch interval in seconds
+  setSyncInterval(20);
 }
 
 void loop() {
-//  // put your main code here, to run repeatedly:
-//  unsigned long time = getTime();
-//  Serial.println(time);
-  if (timeStatus() == timeSet)
+  if (timeStatus() == timeNotSet)
   {
-    timeDisplay();
-//    struct tm * timeinfo;
-//    time_t rawtime(time);
-//    timeinfo = localtime (&rawtime);
-//    Serial.println(asctime(timeinfo));
+    Serial.println("Time not synced");
   }
-  else 
+  else
   {
-    Serial.println("Time not synched");
+    if (timeStatus() == timeNeedsSync)
+      Serial.println("Time need sync");
+    timeDisplay();
   }
   delay(5000);
 }
@@ -69,16 +55,22 @@ void connectWifi()
   if (WiFi.status() == WL_CONNECTED )
     return;
   Serial.print("Connecting to ");
-  Serial.println(ssid);
+  Serial.print(SSID_t);
+  Serial.print(" ");
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID_t, PASSWD_t);
 
+  int count = 0;
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(100);
     Serial.print(".");
+    if ((count++) && (count % 10) == 0)
+    {
+      Serial.print(" ");
+      count = 0;
+    }
   }
-  Serial.println("");
-  Serial.println("Done.");
+  Serial.println(" done.");
 }
 
 time_t getTime()
@@ -87,16 +79,15 @@ time_t getTime()
     connectWifi();
   if (WiFi.status() != WL_CONNECTED)
   {
-    Serial.print("Connectin failed.");
-    delay(1000);
+    Serial.print("ERROR: WiFi connection failed.");
     return 0;
   }
-  // thanks to (http) playground.arduino.cc/Code/NTPclient
+
   static WiFiUDP udp;
-  static int udpInitialized = udp.begin(12399); // Initializes the WiFi UDP library and network settings. Starts WiFiUDP socket, listening at local port 123
+  static int udpInitialized = udp.begin(12670); // Initializes the WiFi UDP library and network settings. Starts WiFiUDP socket, listening at local port 123
   if (0 == udpInitialized) // returns 0 if there are no sockets available to use
   {
-    Serial.println("ERROR: there are no sockets available to use");
+    Serial.println("ERROR: there are no sockets available to use.");
     return 0;
   }
   static char timeServer[] = "pool.ntp.org";  // the NTP server
@@ -107,33 +98,30 @@ time_t getTime()
 
   // Send an NTP request to timeserver on NTP port: 123
   if (! (udp.beginPacket(timeServer, 123)
-         && udp.write((byte *)&ntpFirstFourBytes, 48) == 48
+         && udp.write((byte *)&ntpFirstFourBytes, PKT_LEN) == PKT_LEN
          && udp.endPacket()))
   {
     Serial.println("ERROR: sending request failed");
     return 0; // sending request failed
   }
 
-  // Wait for response; check every pollIntv ms up to maxPoll times
-  const int pollIntv = 150; // poll every this many ms
-  const byte maxPoll = 15;  // poll up to this many times
   int pktLen;               // received packet length
-  for (byte i = 0; i < maxPoll; i++) {
-    if ((pktLen = udp.parsePacket()) == 48)
+  // Wait for NTP server response; check every POLL_INTERVAL ms up to POLL_TIMES times
+  for (byte i = 0; i < POLL_TIMES; i++) {
+    if ((pktLen = udp.parsePacket()) == PKT_LEN)
       break;
-    delay(pollIntv);
+    delay(POLL_INTERVAL);
   }
-  if (pktLen != 48)
+  if (pktLen != PKT_LEN)
   {
     Serial.print("ERROR: no correct packet received; pktLen = ");
-    Serial.println(pktLen);
+    Serial.print(pktLen);
+    Serial.println(", expected 48");
     return 0; // no correct packet received
   }
 
   // Read and discard the first useless bytes
-  // Set useless to 32 for speed; set to 40 for accuracy.
-  const byte useless = 40;
-  for (byte i = 0; i < useless; ++i)
+  for (byte i = 0; i < USELES_BYTES; ++i)
     udp.read();
 
   // Read the integer part of sending time
@@ -146,19 +134,21 @@ time_t getTime()
   // greater than 500ms we round to the next second; we also account
   // for an assumed network delay of 50ms, and (0.5-0.05)*256=115;
   // additionally, we account for how much we delayed reading the packet
-  // since its arrival, which we assume on average to be pollIntv/2.
-  time += (udp.read() > 115 - pollIntv / 8);
+  // since its arrival, which we assume on average to be POLL_INTERVAL/2.
+  time += (udp.read() > 115 - POLL_INTERVAL / 8);
 
   // Discard the rest of the packet
   udp.flush();
+  WiFi.disconnect();
 
-  time -= 2208988800ul;   // convert NTP time to Unix time
-  Serial.print("Got Unix time: ");
-  Serial.println(time);
-//  time_t utc;
-  time = EE.toLocal(time, &tcr);
-  Serial.print("As local time: ");
-  Serial.println(time);
+  // convert NTP time to GMT time
+  time -= 2208988800ul;
+  Serial.print("Got time: ");
+  Serial.print(time); Serial.print(" (Unix:GMT); ");
+  // convert Unix to locale (EET | EEST)
+  time = EasternEuropeanTime.toLocal(time, &tcr);
+  Serial.print(time); Serial.print(" (Unix:"); Serial.print(tcr -> abbrev); Serial.println(")");
+
   return time;
 }
 
@@ -166,20 +156,20 @@ void timeDisplay()
 {
   int day_t = day(), month_t = month(), year_t = year();
   int hour_t = hour(), minute_t = minute(), second_t = second();
-  if(day_t < 10) Serial.print("0");
+  if (day_t < 10) Serial.print("0");
   Serial.print(day_t);
   Serial.print("-");
-  if(month_t < 10) Serial.print("0");
+  if (month_t < 10) Serial.print("0");
   Serial.print(month_t);
   Serial.print("-");
   Serial.print(year_t);
   Serial.print(" ");
   Serial.print(hour_t);
   Serial.print(":");
-  if(minute_t < 10) Serial.print("0");
+  if (minute_t < 10) Serial.print("0");
   Serial.print(minute_t);
   Serial.print(":");
-  if(second_t < 10) Serial.print("0");
+  if (second_t < 10) Serial.print("0");
   Serial.print(second_t);
   Serial.println();
 }
