2dd21cc5c84156d44796185ca51a48f755e9fa01
Liviu Suciu <liviu.suciu@gmail.com>
2017-01-18 16:30:59 +0200

    automatizare_cpp

---
+++ automatizare_cpp/ProgrammP1.cpp
@@ -0,0 +1,26 @@
+/*
+ * ProgrammP1.cpp
+ *
+ *  Created on: Jan 18, 2017
+ *      Author: lsuciu
+ */
+
+#include "ProgrammP1.h"
+
+#include "LsuTimeHelper.h"
+
+const String ProgrammP1::START_HOUR_P1 = "start_hour_p1";
+const String ProgrammP1::START_MINUTE_P1 = "start_minute_p1";
+const String ProgrammP1::STOP_HOUR_P1 = "stop_hour_p1";
+const String ProgrammP1::STOP_MINUTE_P1 = "stop_minute_p1";
+const String ProgrammP1::TARGET_TEMPERATURE_P1 = "target_temperature_p1";
+
+bool ProgrammP1::shouldRun(int temperature) {
+	int target_temperature = target_temperature_p1;
+	if (running)
+		target_temperature += delta_running_temperature_p1;
+	return temperature <= target_temperature
+			&& LsuTimeHelper::isNowBetween(start_hour_p1, start_minute_p1,
+					stop_hour_p1, stop_minute_p1);
+}
+
---
+++ automatizare_cpp/ProgrammP1.h
@@ -0,0 +1,45 @@
+/*
+ * ProgrammP1.h
+ *
+ *  Created on: Jan 18, 2017
+ *      Author: lsuciu
+ */
+
+#ifndef PROGRAMMP1_H_
+#define PROGRAMMP1_H_
+
+#include <Arduino.h>
+
+class ProgrammP1 {
+public:
+	// constants
+	static const String START_HOUR_P1;
+	static const String START_MINUTE_P1;
+	static const String STOP_HOUR_P1;
+	static const String STOP_MINUTE_P1;
+	static const String TARGET_TEMPERATURE_P1;
+public:
+	ProgrammP1(int target_temperature = 2100, byte start_hour = 15,
+			byte start_minute = 0, byte stop_hour = 7, byte stop_minute = 0, int delta_running_temperature = 10) :
+			target_temperature_p1(target_temperature), start_hour_p1(
+					start_hour), start_minute_p1(start_minute), stop_hour_p1(
+					stop_hour), stop_minute_p1(stop_minute), delta_running_temperature_p1(delta_running_temperature), running(false) {
+	}
+	virtual ~ProgrammP1() {
+	}
+
+	bool shouldRun(int temperature);
+private:
+	// fields
+	byte start_hour_p1;
+	byte start_minute_p1;
+	byte stop_hour_p1;
+	byte stop_minute_p1;
+	int target_temperature_p1;
+	int delta_running_temperature_p1;
+
+	// running states
+	bool running;
+};
+
+#endif /* PROGRAMMP1_H_ */
