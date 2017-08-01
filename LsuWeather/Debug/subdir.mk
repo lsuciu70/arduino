a04ef867130df8edb4011982191279b2eb153d38
Liviu Suciu <liviu.suciu@gmail.com>
2017-08-01 08:20:17 +0300

    nc

--- LsuWeather/Debug/subdir.mk
+++ LsuWeather/Debug/subdir.mk
@@ -27,14 +27,14 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++98 -DARDUINO=100 -DUBRRH -I/home/lsuciu/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/cores/esp8266 -I/home/lsuciu/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/libraries/Wire -I/home/lsuciu/arduino/libraries/ESP8266WiFi/src -I/home/lsuciu/arduino/libraries/SparkFunBME280/src -I/home/lsuciu/arduino/libraries/LsuWiFi -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++98 -DARDUINO=100 -DUBRRH -I/home/lsuciu/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/cores/esp8266 -I/home/lsuciu/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/libraries/Wire -I/home/lsuciu/arduino/libraries/ESP8266WiFi/src -I/home/lsuciu/arduino/libraries/SparkFun_BME280/src -I/home/lsuciu/arduino/libraries/LsuWiFi -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 %.o: ../%.ino
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++98 -DARDUINO=100 -DUBRRH -I/home/lsuciu/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/cores/esp8266 -I/home/lsuciu/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/libraries/Wire -I/home/lsuciu/arduino/libraries/ESP8266WiFi/src -I/home/lsuciu/arduino/libraries/SparkFunBME280/src -I/home/lsuciu/arduino/libraries/LsuWiFi -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++98 -DARDUINO=100 -DUBRRH -I/home/lsuciu/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/cores/esp8266 -I/home/lsuciu/.arduino15/packages/esp8266/hardware/esp8266/2.3.0/libraries/Wire -I/home/lsuciu/arduino/libraries/ESP8266WiFi/src -I/home/lsuciu/arduino/libraries/SparkFun_BME280/src -I/home/lsuciu/arduino/libraries/LsuWiFi -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
