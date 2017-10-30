e4f2712beddd9e12176fb7fa997adf3e09852e2f
Liviu Suciu <liviu.suciu@gmail.com>
2017-10-30 14:53:05 +0200

    nc

--- automatizare_2/LsuHeating.h
+++ automatizare_2/LsuHeating.h
@@ -25,13 +25,15 @@ namespace
 // floor section
 // parter
 const char* T_LOC_PARTER = "parter";
-const char* MAC_PARTER = "18:FE:34:D4:0D:EC"; // old
+//const char* MAC_PARTER = "18:FE:34:D4:0D:EC"; // old
 //const char* MAC_PARTER = "5C:CF:7F:EF:BE:50"; // new
+const char* MAC_PARTER = "5C:CF:7F:EF:B4:0B"; // other
+
 // etaj
 const char* T_LOC_ETAJ = "etaj";
 const char* MAC_ETAJ = "5C:CF:7F:88:EE:49";
 
-char T_LOC[7] = {'\0'};
+char t_loc[7] = "-";
 // end floor section
 
 // WiFi section
@@ -42,8 +44,10 @@ const char* SSID_PASSWDs[SSID_SIZE] =
 { "r4cD7TPG", "r4cD7TPG", "r4cD7TPG" };
 
 const IPAddress master_server_ip(192, 168, 100, 100);
+
 const int master_server_port = 8081;
-const char* page = "/update.php";
+const char* update_page = "/update.php";
+const char* log_page = "/log_save.php";
 // end WiFi section
 
 // NTP time section
@@ -75,13 +79,13 @@ const uint8_t SENZOR_ADDRESS_LENGTH = 8;
 const uint8_t SENZOR_ADDRESS[2 * SENZOR_COUNT][SENZOR_ADDRESS_LENGTH] =
 {
 { 0x28, 0xFF, 0x9F, 0x1C, 0xA6, 0x15, 0x04, 0xEF }, // s0
-    { 0x28, 0xFF, 0x09, 0x4F, 0xA6, 0x15, 0x04, 0x94 }, // s1
-    { 0x28, 0xFF, 0x18, 0x1A, 0xA6, 0x15, 0x03, 0xFF }, // s2
-    { 0x28, 0xFF, 0xDC, 0x0A, 0xA6, 0x15, 0x03, 0xFA }, // s3
-    { 0x28, 0xFF, 0x40, 0x09, 0xA6, 0x15, 0x03, 0xE4 }, // j0
-    { 0x28, 0xFF, 0x21, 0x14, 0xA6, 0x15, 0x03, 0x9D }, // j1
-    { 0x28, 0xFF, 0xCE, 0x1C, 0xA6, 0x15, 0x04, 0xB7 }, // j2
-    { 0x28, 0xFF, 0x37, 0x1A, 0xA6, 0x15, 0x03, 0x68 }, // j3
+  { 0x28, 0xFF, 0x09, 0x4F, 0xA6, 0x15, 0x04, 0x94 }, // s1
+  { 0x28, 0xFF, 0x18, 0x1A, 0xA6, 0x15, 0x03, 0xFF }, // s2
+  { 0x28, 0xFF, 0xDC, 0x0A, 0xA6, 0x15, 0x03, 0xFA }, // s3
+  { 0x28, 0xFF, 0x40, 0x09, 0xA6, 0x15, 0x03, 0xE4 }, // j0
+  { 0x28, 0xFF, 0x21, 0x14, 0xA6, 0x15, 0x03, 0x9D }, // j1
+  { 0x28, 0xFF, 0xCE, 0x1C, 0xA6, 0x15, 0x04, 0xB7 }, // j2
+  { 0x28, 0xFF, 0x37, 0x1A, 0xA6, 0x15, 0x03, 0x68 }, // j3
     };
 
 int temperature[SENZOR_COUNT];
@@ -116,8 +120,6 @@ bool is_running = false;
 
 void relayInitialize()
 {
-  if (isRelayInitialized)
-    return;
   for (uint8_t i = 0; i < SENZOR_COUNT; ++i)
   {
     temperature[i] = 0;
@@ -125,7 +127,9 @@ void relayInitialize()
     digitalWrite(relay[i], HIGH);
     delay(25);
   }
-  isRelayInitialized = true;
+#if DEBUG
+  Serial.println("Relay initialized");
+#endif
 }
 
 bool isNowAfter(byte start_h, byte start_m)
@@ -141,22 +145,22 @@ int floatToRound05Int(float temp)
   uint8_t mod = t % 10;
   switch (mod)
   {
-  case 1:
-  case 2:
-    t += (0 - mod);
-    break;
-  case 3:
-  case 4:
-  case 6:
-  case 7:
-    t += (5 - mod);
-    break;
-  case 8:
-  case 9:
-    t += (10 - mod);
-    break;
-  default:
-    break;
+    case 1:
+    case 2:
+      t += (0 - mod);
+      break;
+    case 3:
+    case 4:
+    case 6:
+    case 7:
+      t += (5 - mod);
+      break;
+    case 8:
+    case 9:
+      t += (10 - mod);
+      break;
+    default:
+      break;
   }
   return t;
 }
@@ -166,7 +170,7 @@ void pritSerial()
 #if DEBUG
   Serial.print(LsuNtpTime::datetimeString(datetimebuff));
   Serial.print(" - [");
-  Serial.print(T_LOC);
+  Serial.print(t_loc);
   Serial.print("] ");
   for (uint8_t i = 0; i < SENZOR_COUNT; ++i)
   {
@@ -204,26 +208,27 @@ void startConversion_4()
   startConversion(3);
 }
 
-void sendPostData(const char* data, const char* page,
-    const IPAddress & server, const int port)
+void sendPostData(const char* data, const char* page)
 {
   const char* post_data_fmt = "t_loc=%s&%s";
-  const size_t post_data_len = strlen(post_data_fmt) - 4 + strlen(T_LOC) + strlen(data);
+  const size_t post_data_len = strlen(post_data_fmt) - 4 + strlen(t_loc)
+      + strlen(data);
   char post_data[post_data_len + 1];
-  sprintf(post_data, post_data_fmt, T_LOC, data);
+  sprintf(post_data, post_data_fmt, t_loc, data);
 //  String post_data = String(T_LOC_NAME) + "=" + T_LOC + "&" + data;
 
-  const char* post_req_fmt =
-      "POST %s HTTP/1.1\r\n"
+  const char* post_req_fmt = "POST %s HTTP/1.1\r\n"
       "Host: %s\r\n"
       "User-Agent: Arduino/1.0\r\n"
       "Connection: close\r\n"
       "Content-Type: application/x-www-form-urlencoded\r\n"
       "Content-Length: %d\r\n";
-  const size_t post_req_len = strlen(post_req_fmt) - 6 + strlen(page) + 15 + 4;
-  char post_req[post_req_len];
+  const size_t post_req_len = strlen(post_req_fmt) - 6 + strlen(page)
+      + IP_ADDR_STR_LEN + 3;
+  char post_req[post_req_len + 1];
   char addr[IP_ADDR_STR_LEN + 1];
-  sprintf(post_req, post_req_fmt, page, LsuWiFi::ipAddressStr(addr), post_data_len);
+  sprintf(post_req, post_req_fmt, page, LsuWiFi::ipAddressStr(addr),
+      post_data_len);
 
 //  String post_req = String("") + "POST " + page + " HTTP/1.1\r\n" + "Host: "
 //      + WiFi.localIP().toString() + "\r\n" + "User-Agent: Arduino/1.0\r\n"
@@ -231,32 +236,46 @@ void sendPostData(const char* data, const char* page,
 //      + "Content-Type: application/x-www-form-urlencoded\r\n"
 //      + "Content-Length: " + post_data_len + "\r\n";
   WiFiClient client;
-  if (client.connect(server, port))
+  if (client.connect(master_server_ip, master_server_port))
   {
     client.println(post_req);
     client.println(post_data);
     delay(10);
     client.stop();
   }
+#if DEBUG
   else
   {
-//    writeLogger(
-//        String("[") + T_LOC + "]  ERROR connecting server: " + IPAddress(server).toString());
+    char addr_str[IP_ADDR_STR_LEN + 1];
+    LsuWiFi::ipAddressStr(master_server_ip, addr_str);
   }
+#endif
+}
+
+/**
+ * Adds date and time, and location to given message and sends it to log_save page.
+ */
+void writeLogger(const char* what)
+{
+  char* datetime = LsuNtpTime::datetimeString(datetimebuff);
+  const char* post_data_fmt = "t_log=%s - [%s] %s";
+  size_t post_data_len = strlen(post_data_fmt) - 6 + strlen(datetime)
+      + strlen(t_loc) + strlen(what);
+  char post_data[post_data_len + 1];
+  sprintf(post_data, post_data_fmt, datetime, t_loc, what);
+  sendPostData(post_data, log_page);
 }
 
 void sendCurrentTemperatures()
 {
-  const char* msg_fmt = "t_%d=%d&t_%d_r=%d&t_%d=%d&t_%d_r=%d&t_%d=%d&t_%d_r=%d&t_%d=%d&t_%d_r=%d";
+  const char* msg_fmt =
+      "t_%d=%d&t_%d_r=%d&t_%d=%d&t_%d_r=%d&t_%d=%d&t_%d_r=%d&t_%d=%d&t_%d_r=%d";
   size_t msg_len = strlen(msg_fmt) - 32 + 24 + 4;
   char msg[msg_len + 1];
   short run_flag = is_running ? 1 : 0;
-  sprintf(msg, msg_fmt
-      ,0 ,temperature[0], 0, run_flag
-      ,1 ,temperature[1], 1, run_flag
-      ,2 ,temperature[2], 2, run_flag
-      ,3 ,temperature[3], 3, run_flag);
-  sendPostData(const_cast<const char*>(msg), page, master_server_ip, master_server_port);
+  sprintf(msg, msg_fmt, 0, temperature[0], 0, run_flag, 1, temperature[1], 1,
+      run_flag, 2, temperature[2], 2, run_flag, 3, temperature[3], 3, run_flag);
+  sendPostData(const_cast<const char*>(msg), update_page);
 }
 
 void checkProgramming()
@@ -364,17 +383,8 @@ void updateTemperature()
 void setup()
 {
 #if DEBUG
-  Serial.begin(460800);
+  Serial.begin(115200);
   Serial.println();
-#endif
-  for (uint8_t idx = 0; idx < SSID_SIZE; ++idx)
-  {
-    LsuWiFi::addAp(SSIDs[idx], SSID_PASSWDs[idx]);
-  }
-  LsuWiFi::connect();
-  LsuNtpTime::begin();
-#if DEBUG
-  Serial.printf("Got NTP time: %s\n", LsuNtpTime::datetimeString(datetimebuff));
 #endif
 
   char mac[MAC_ADDR_STR_LEN + 1];
@@ -382,30 +392,58 @@ void setup()
   if (strcmp(mac, MAC_ETAJ) == 0)
   {
     offset = 0;
-    strcpy(T_LOC, T_LOC_ETAJ);
+    strcpy(t_loc, T_LOC_ETAJ);
   }
   else if (strcmp(mac, MAC_PARTER) == 0)
   {
     offset = SENZOR_COUNT;
-    strcpy(T_LOC, T_LOC_PARTER);
+    strcpy(t_loc, T_LOC_PARTER);
   }
   else
+  {
+    const char* msg_fmt = "Unknown MAC address: %s";
+    const size_t msg_len = strlen(msg_fmt) - 2 + strlen(mac);
+    char msg[msg_len + 1];
+    sprintf(msg, msg_fmt, mac);
+
+    writeLogger(msg);
+
     while (true)
     {
 #if DEBUG
-      Serial.println(String("Unknown MAC address: ") + mac);
+      Serial.println(msg);
 #endif
       delay(20 * 60 * SECOND);
     }
+  }
+
+  for (size_t idx = 0; idx < SSID_SIZE; ++idx)
+  {
+    LsuWiFi::addAp(SSIDs[idx], SSID_PASSWDs[idx]);
+  }
+  LsuWiFi::connect(2);
+  LsuNtpTime::begin();
+#if DEBUG
+  Serial.printf("Got NTP time: %s\n", LsuNtpTime::datetimeString(datetimebuff));
+#endif
+
+  const char* SSID = LsuWiFi::currentSSID();
+  const char* ip_msg_fmt = "WiFi: conectat la %s, adresa IP: %s";
+  const size_t ip_msg_len = strlen(ip_msg_fmt) - 4 + strlen(SSID) + IP_ADDR_STR_LEN;
+  char ip_msg[ip_msg_len + 1];
+  char addr_str[IP_ADDR_STR_LEN + 1];
+  sprintf(ip_msg, ip_msg_fmt, SSID, LsuWiFi::ipAddressStr(addr_str));
+  writeLogger(ip_msg);
+
 
   // update has run today
   int minutes_now = hour() * 60 + minute();
   int minutes_start = start_hour_p2 * 60 + start_minute_p2;
-  has_p2_run_today = minutes_start - minutes_now > 60;
+  has_p2_run_today = minutes_now - minutes_start > 60;
 #if DEBUG
-      Serial.printf("Mark has_tun_today: %s\n", (has_p2_run_today ? "true" : "false"));
+  Serial.printf("Mark has_tun_today: %s, minutes now: %d, minutes start: %d\n",
+      (has_p2_run_today ? "true" : "false"), minutes_now, minutes_start);
 #endif
-
 
   // Start up the temperature library
   dallasTemperature1st_pin0.begin();
@@ -415,16 +453,19 @@ void setup()
   dallasTemperature2nd_pin2.begin();
   dallasTemperature2nd_pin2.setResolution(RESOLUTION);
   dallasTemperature2nd_pin2.setWaitForConversion(false);
+#if DEBUG
+  Serial.println("Temperature sensors initialized");
+#endif
 
-  int first_read = millis() + 8 * CONVERSION_TIME;
-  scheduleAt(first_read);
-//  LsuScheduler::add(relayInitialize, millis() + 5);
+  LsuScheduler::add(relayInitialize, millis() + 10);
+  scheduleAt(millis() + 8 * CONVERSION_TIME);
+#if DEBUG
+  Serial.println("Setup done");
+#endif
 }
 
 void loop()
 {
-  if (!isRelayInitialized)
-    relayInitialize();
   LsuScheduler::execute(millis());
 }
 
